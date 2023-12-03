291adb69b30f7177200143bdd145d41bdf37a819
Liberbell <liberbell@gmail.com>
2023-12-04 08:32:56 +0900

    scatter3

--- work1/Dash/scatter.py
+++ work1/Dash/scatter.py
@@ -3,6 +3,9 @@ from dash import callback, html, dcc, Input, Output, State
 import numpy as np
 import plotly.graph_objects as go
 
+x1 = np.random.randint(0, 100, 50)
+y1 = np.random.randint(0, 100, 50)
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
