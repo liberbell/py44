b416056ddbac724a0a3f880b4fa62a747b641b09
Liberbell <liberbell@gmail.com>
2023-12-04 08:55:12 +0900

    line4

--- work1/Dash/line.py
+++ work1/Dash/line.py
@@ -9,7 +9,20 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
+app.layout = html.Div([
+    dcc.Graph(
+        id="sample-line",
+        figure={
+            "data":[
+                go.Scatter(
+                    x=df["date"],
+                    y=df["MSFT"],
+                    mode="lines",
+                )
+            ]
+        }
+    )
+])
 
 
 if __name__ == '__main__':
