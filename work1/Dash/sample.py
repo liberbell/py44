0053cfafcaea7bf1b57cf7c7afea3822db641464
Liberbell <liberbell@gmail.com>
2023-12-03 14:37:14 +0900

    sample4

--- work1/Dash/sample.py
+++ work1/Dash/sample.py
@@ -1,6 +1,7 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import callback, html, dcc, Input, Output, State
+# import dash_core_components as dcc
+# import dash_html_components as html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
