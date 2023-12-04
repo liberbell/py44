68b88d1b039d1b72979eadc80ed088bed8738a07
Liberbell <liberbell@gmail.com>
2023-12-05 08:37:24 +0900

    markdown23

--- work1/Dash/markdown2.py
+++ work1/Dash/markdown2.py
@@ -15,6 +15,16 @@ app.layout = html.Div([
             {"label": "Alex", "value": "alex"},
         ],
         value="eric"
+    ),
+    html.Label("Multi select dropdown"),
+    dcc.Dropdown(
+        options=[
+            {"label": "Bob", "value": "bob"},
+            {"label": "Eric", "value": "eric"},
+            {"label": "Alex", "value": "alex"},
+        ],
+        value=["eric", "alex"],
+        multi=True
     )
 ])
 
