5dd48bf561e62ba443ce719467feabc8adf1a414
Liberbell <liberbell@gmail.com>
2023-12-05 08:29:22 +0900

    markdown1

---
+++ work1/Dash/markdown.py
@@ -0,0 +1,21 @@
+import dash
+from dash import callback, html, dcc, Input, Output, State
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Markdown("""
+# Title 1
+# Title 2
+
+- sub title
+- sub title
+- sub title
+""")
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
