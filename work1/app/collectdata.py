c96d0ba64505865bf277c3358b9566c927d7fce8
Liberbell <liberbell@gmail.com>
2023-12-06 08:48:16 +0900

    collectdata2

--- work1/app/collectdata.py
+++ work1/app/collectdata.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://scraping-for-begginer.herokuapp.com/udemy"
+URL = "https://scraping-for-beginner.herokuapp.com/udemy"
+data = requests.get(URL)
+print(data.status_code)
