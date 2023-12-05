659e12755ad2be56e043f1a1ead3cabe98856b7e
Liberbell <liberbell@gmail.com>
2023-12-06 08:30:43 +0900

    callback2

--- work1/Dash/callback.py
+++ work1/Dash/callback.py
@@ -7,7 +7,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-
+    dcc.Input(id="input-div", value="Initial value", type="text"),
+    html.Div(id="output-div")
 ])
 
 
