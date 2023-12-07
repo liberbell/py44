9c9e3f58b9373dfc36838eaf6f1ef79014016bb2
Liberbell <liberbell@gmail.com>
2023-12-08 08:56:01 +0900

    view1

---
+++ work1/app/view.py
@@ -0,0 +1,17 @@
+import dash
+from dash import callback, html, dcc, Input, Output, State
+import plotly.graph_objects as go
+import pandas as pd
+import datetime
+
+
+df = pd.read_csv("time_series.csv")
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
