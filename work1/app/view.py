b4fddd3157ccb25a34171a5c76026ae5007cabfa
Liberbell <liberbell@gmail.com>
2023-12-09 14:42:35 +0900

    view1

--- work1/app/view.py
+++ work1/app/view.py
@@ -11,7 +11,10 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
+dates = []
+for _date in df["date"]:
+    date = datetime.datetime.strptime(_date, "%Y/%m/%d").date()
+    dates.append(date)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
