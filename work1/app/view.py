31747dde93bbcbd003dfd14badf834c4e90cf30b
Liberbell <liberbell@gmail.com>
2023-12-09 15:06:15 +0900

    view8

--- work1/app/view.py
+++ work1/app/view.py
@@ -37,13 +37,19 @@ app.layout = html.Div(children=[
                         opacity=0.7,
                         yaxis="y1"
                     ),
-                    go.Bar(
-                        x=dates,
-                        y=num_of_students,
-                        name="Subscribers diff",
-                        yaxis="y2"
-                    )
-                ]
+                    # go.Bar(
+                    #     x=dates,
+                    #     y=num_of_students,
+                    #     name="Subscribers diff",
+                    #     yaxis="y2"
+                    # )
+                ],
+                "layout": go.Layout(
+                    title = "Subscriber sum",
+                    xaxis = dict(title="Date"),
+                    yaxis = dict(title="Subscribers", side="left",
+                                 showgrid=False, range=[2500, max(num_of_students)+100])
+                )
             }
         )
     ])
