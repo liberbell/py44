93f9114a17eedb4761cc7af3995062902a673bf1
Liberbell <liberbell@gmail.com>
2023-12-10 17:09:47 +0900

    database3

--- work1/app/assets/database.py
+++ work1/app/assets/database.py
@@ -11,3 +11,10 @@ db_session = scoped_session(sessionmaker(
     autoflush=False,
     bind=engine
 ))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    import assets.models
+    Base.metadata.create_all(bind=engine)
