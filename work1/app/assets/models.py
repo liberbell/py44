9479f3492a4d05426ecf392ddd47cf9595b36891
Liberbell <liberbell@gmail.com>
2023-12-10 17:13:04 +0900

    models1

---
+++ work1/app/assets/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Date
+from assets.database import Base
+from datetime import datetime as dt
+
+class Data(Base):
+    __tablename__ = "data"
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, unique=False)
+    subscribers = Column(Integer, unique=False)
+    reviews = Column(Integer, unique=False)
+    timestamp = Column(DateTime, default=dt.now())
+
+    def __init__(self, date=None, subscribers=None, reviews=None, timestamp=None):
+        self.date = date
+        self.subscribers = subscribers
+        self.reviews = reviews
+        self.timestamp = timestamp
