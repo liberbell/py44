5400f989920704ae435816c0d0c68fc349412bca
Liberbell <liberbell@gmail.com>
2023-12-10 17:07:56 +0900

    database2

--- work1/app/assets/database.py
+++ work1/app/assets/database.py
@@ -3,3 +3,11 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
 import os
+
+database_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data.db")
+engine = create_engine("sqlite:///" + database_file, convert_unicode=True, echo=True)
+db_session = scoped_session(sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+))
