f6cb0391c3e5c3f7e8fc9932961e3ff604520e3c
Liberbell <liberbell@gmail.com>
2023-12-10 17:24:01 +0900

    database4

--- work1/app/assets/database.py
+++ work1/app/assets/database.py
@@ -6,11 +6,13 @@ import os
 
 database_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data.db")
 engine = create_engine("sqlite:///" + database_file, convert_unicode=True, echo=True)
-db_session = scoped_session(sessionmaker(
+db_session = scoped_session(
+    sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
-))
+    )
+)
 
 Base = declarative_base()
 Base.query = db_session.query_property()
