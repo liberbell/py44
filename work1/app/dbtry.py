47730e330ed493d9c9bb9d6bbbb0ffa6b64ce40a
Liberbell <liberbell@gmail.com>
2023-12-12 08:43:22 +0900

    dbtry7

--- work1/app/dbtry.py
+++ work1/app/dbtry.py
@@ -15,3 +15,6 @@ print(type(date))
 for index, _df in df.iterrows():
     date = datetime.datetime.strptime(_df["date"], "%Y/%m/%d").date()
     row = models.Data(date=date, subscribers=_df["subscribers"], reviews=_df["reviews"])
+    db_session.add(row)
+
+db_session.commit()
