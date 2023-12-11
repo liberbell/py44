61da06cf59e100ca25f4bf6adb2e13506caf15b6
Liberbell <liberbell@gmail.com>
2023-12-12 08:46:21 +0900

    database1

--- work1/app/assets/database.py
+++ work1/app/assets/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
+import pandas as pd
 import os
 
 database_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data.db")
@@ -20,3 +21,14 @@ Base.query = db_session.query_property()
 def init_db():
     import assets.models
     Base.metadata.create_all(bind=engine)
+
+def read_data():
+    from assets import models
+    df = pd.read_csv("assets/data.csv")
+
+    for index, _df in df.iterrows():
+        date = datetime.datetime.strptime(_df["date"], "%Y/%m/%d").date()
+        row = models.Data(date=date, subscribers=_df["subscribers"], reviews=_df["reviews"])
+        db_session.add(row)
+
+    db_session.commit()
