46e81a64f108ddab94b6887e5245063f3be617e4
Liberbell <liberbell@gmail.com>
2023-12-12 08:35:48 +0900

    dbtry2

--- work1/app/dbtry.py
+++ work1/app/dbtry.py
@@ -2,6 +2,12 @@ from assets import models
 import pandas as pd
 
 from assets.database import db_session
+import datetime
 
 df = pd.read_csv("assets/data.csv")
 print(df.head())
+
+print(type(df.iloc[0,0]))
+date = datetime.datetime.strptime(df.iloc[0,0], "%Y/%m/%d").date()
+print(date)
+print(type(date))
